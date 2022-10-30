37cc6c6f1f30e73c260b774a4beb1b58a7680dde
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-30 21:00:05 +0100

    expose id

--- src/main/java/com/lambarki/yassine/spring_cloud_gateway/SpringGloudGatewayApplication.java
+++ src/main/java/com/lambarki/yassine/spring_cloud_gateway/SpringGloudGatewayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class SpringGloudGatewayApplication {
@@ -15,7 +16,8 @@ public class SpringGloudGatewayApplication {
     }
 
     @Bean
-    CommandLineRunner start(CustomerRepository customerRepository) {
+    CommandLineRunner start(CustomerRepository customerRepository, RepositoryRestConfiguration repositoryRestConfiguration) {
+        repositoryRestConfiguration.exposeIdsFor(Customer.class);
         return args -> {
             customerRepository.save(new Customer(null, "Mouad", "mouad@gmail.com"));
             customerRepository.save(new Customer(null, "Yassine", "yassine@gmail.com"));
