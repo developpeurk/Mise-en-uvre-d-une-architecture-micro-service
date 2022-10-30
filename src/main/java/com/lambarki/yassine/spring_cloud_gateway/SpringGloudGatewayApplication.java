e2a43af1036b50c119e8ed97a57bd5678587b40d
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-10-30 18:39:39 +0100

    first commit

---
+++ src/main/java/com/lambarki/yassine/spring_cloud_gateway/SpringGloudGatewayApplication.java
@@ -0,0 +1,29 @@
+package com.lambarki.yassine.spring_cloud_gateway;
+
+import com.lambarki.yassine.spring_cloud_gateway.entities.Customer;
+import com.lambarki.yassine.spring_cloud_gateway.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringGloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGloudGatewayApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(new Customer(null, "Mouad", "mouad@gmail.com"));
+            customerRepository.save(new Customer(null, "Yassine", "yassine@gmail.com"));
+            customerRepository.save(new Customer(null, "Salim", "salim@gmail.com"));
+
+            customerRepository.findAll().forEach(customer -> {
+                System.out.println(customer.toString());
+            });
+        };
+    }
+}
